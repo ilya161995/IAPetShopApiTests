f50e43bd8e04dc8d83e49fd51785253fb52162f4
Илья <ilya161995@gmail.com>
2026-02-12 07:34:57 +0400

    Добавил тест "Получение списка питомцев по статусу"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -157,4 +157,26 @@ class TestPet:
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 404
 
+    @allure.title("Получение списка питомцев по статусу")
+    @pytest.mark.parametrize(
+        "status, expected_status_code",
+        [
+            ("available", 200),
+            ("pending", 200),
+            ("sold", 200),
+            ("", 400),
+            ("nhbn", 400)
+        ]
+
+    )
+    def test_get_pets_by_status(self, status, expected_status_code):
+        with allure.step(f"Отправка запроса на получение питомцев по статусу {status}"):
+            response = requests.get(f"{BASE_URL}/pet/findByStatus", params={"status": status})
+
+        with allure.step("Проверка статуса ответа и формата данных"):
+            assert response.status_code == expected_status_code
+            assert isinstance(response.json(), list)
+
+
+
 
